90d092b1bcdffbd7f37963836ed70e492a389c36
Satish <satish2694@users.noreply.github.com>
2026-03-18 11:07:04 +0530

    Initial commit: Spring Cloud Config Client microservice with inventory-service

---
+++ src/main/java/com/microservice/Microservice1/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.microservice.Microservice1.controller;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/serviceA")
+public class HelloController {
+    @GetMapping("/displayMessage")
+    public ResponseEntity<String> showMessage(){
+        return ResponseEntity.ok("Microservice 1 controller executed");
+    }
+}
